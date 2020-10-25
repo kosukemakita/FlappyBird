52baeb5104709bdf498cfbdeb75beca86b49e566
kosukemakita <makitakosuke0114@gmail.com>
2020-10-25 15:31:56 +0900

    画像追加と背景設定

---
+++ GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by 蒔田幸祐 on 2020/10/25.
+//  Copyright © 2020 kousuke.makita. All rights reserved.
+//
+
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        // 背景色を設定
+        backgroundColor = UIColor(red: 0.15, green: 0.75, blue: 0.90, alpha: 1)
+    }
+}
